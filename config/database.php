c32202b09f33626ba46831730f28f7b41094b58b
zhengluming <908235332@qq.com>
2017-09-13 13:56:13 +0800

    更新配置

--- config/database.php
+++ config/database.php
@@ -5,20 +5,25 @@
  */
 
 return [
-    //连接名称 => 连接配置
-    'bbs' => [
-        'class' => LumengPHP\Db\Connection\SimpleConnection::class,
-        //数据库类型：mysql、pgsql、sqlsrv等
-        'type' => 'mysql',
-        //表前缀，如：bbs_
-        'tablePrefix' => 'bbs_',
-        //数据库字符集
-        'charset' => 'utf8',
-        //数据库配置
-        'host' => env('DB_HOST', '127.0.0.1'),
-        'port' => env('DB_PORT', 3306),
-        'dbName' => env('DB_NAME', 'bbs'),
-        'username' => env('DB_USERNAME', 'bbs'),
-        'password' => env('DB_PASSWORD', 'bbs'),
+    //日志组件名称
+    'logger' => env('DB_LOGGER', 'dbLogger'),
+    //连接配置
+    'connections' => [
+        //连接名称 => 连接配置
+        'bbs' => [
+            'class' => LumengPHP\Db\Connection\SimpleConnection::class,
+            //数据库类型：mysql、pgsql、sqlsrv等
+            'type' => 'mysql',
+            //表前缀，如：bbs_
+            'tablePrefix' => 'bbs_',
+            //数据库字符集
+            'charset' => 'utf8',
+            //数据库配置
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', 3306),
+            'dbName' => env('DB_NAME', 'bbs'),
+            'username' => env('DB_USERNAME', 'bbs'),
+            'password' => env('DB_PASSWORD', 'bbs'),
+        ],
     ],
 ];
